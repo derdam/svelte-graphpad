ebb7ceb78080696535455ca0927583c732c8fc29
IT-DEV <it-dev@MAMISAMACFANs-MacBook-Air.local>
2020-01-20 21:34:37 +0100

    change node class

---
+++ src/NodeView.js
@@ -0,0 +1,28 @@
+
+let views = {
+    Document: {view: { shape:'image' }, props: [{image:'./contract-signing.png'}, { size:45}]}
+
+}
+
+export const buildNodeView = (node) => {
+    if (node.nodeClass) {
+        let view = views[node.nodeClass] ? views[node.nodeClass].view : {}
+       
+        let ret = {...view}
+        
+        let props = views[node.nodeClass] ? views[node.nodeClass].props : [] 
+            props.forEach(p => {
+              
+                let prop = Object.keys(p)[0];
+                console.log(prop);
+               if (node[prop] === undefined) {
+                ret = {...ret, ...p}
+            }
+            }); 
+            return ret;
+        } 
+    else { 
+        return {} 
+    }
+   
+} ;
