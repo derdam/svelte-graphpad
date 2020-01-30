ab36a3b6cd104260b5a314dfae4c8984eadd42a8
IT-DEV <it-dev@MAMISAMACFANs-MacBook-Air.local>
2020-01-30 23:46:50 +0100

    commit

--- src/GraphData.svelte
+++ src/GraphData.svelte
@@ -41,13 +41,70 @@
     }
    }
   
-  
+  function download(filename, text) {
+      var element = document.createElement('a');
+      element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+      element.setAttribute('download', filename);
+
+      element.style.display = 'none';
+      document.body.appendChild(element);
+
+      element.click();
+
+      document.body.removeChild(element);
+  }
+
+  function downloadCurrent() {
+      download("GraphPad-"+storeName+".json",JSON.stringify($graph));
+   }
+
+    let files = [];
+    const changed = (event)=>{
+           
+            files = event.target.files;
+
+            // Setup file reading
+            var reader = new FileReader();
+            reader.onload = handleFileRead;
+
+             function handleFileRead(event) {
+                   // var save = JSON.parse(event.target.result);
+                      localStorage.setItem(file.name, event.target.result);
+                      storeGen++;
+                      
+                }
+               let file = files[0];
+               // console.log(file.name); 
+
+                reader.readAsText(file); // fires onload when done.
+        }
+
+// Start file download.
+//download("hello.txt","This is the content of my file :)");
+
 </script>
-<p>Store name</p><input type="text" bind:value={storeName}>
-<p><button on:click={store}>Store</button>
-<button on:click={deleteEntry}>Delete</button></p>
+<p>Store name</p><input type="text" bind:value={storeName}/>
+<p>
+   <button on:click={store}>Save</button>&nbsp;
+   <button on:click={deleteEntry}>Delete</button>&nbsp;
+   <button on:click={downloadCurrent}>Download</button>
+  
+</p>
+ <input
+      type="file"
+      accept="application/JSON"
+      class="w-full h-full cursor-pointer"
+      bind:files
+      on:change={changed}
+      />
 <!-- <p>{JSON.stringify($graph)}</p> -->
 <p>Stores</p>
 {#each storeKeys as key} 
    <button on:click={restore2(key)}>{key}</button>&nbsp;
 {/each}
+
+
+
+
+
+
--- src/NodeEditorDocument.svelte
+++ src/NodeEditorDocument.svelte
@@ -6,11 +6,40 @@
 
     $: { updateNode(node); console.log("updateNode"); }
 
+
+function toDataURL(src, callback, outputFormat) {
+  var img = new Image();
+  img.crossOrigin = 'Anonymous';
+  img.onload = function() {
+    var canvas = document.createElement('CANVAS');
+    var ctx = canvas.getContext('2d');
+    var dataURL;
+    canvas.height = this.naturalHeight;
+    canvas.width = this.naturalWidth;
+    ctx.drawImage(this, 0, 0);
+    dataURL = canvas.toDataURL(outputFormat);
+    callback(dataURL);
+  };
+  img.src = src;
+  if (img.complete || img.complete === undefined) {
+    img.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+    img.src = src;
+  }
+}
+
     let files = [];
     const changed = (event)=>{
            
             files = event.target.files;
             node.image = window.URL.createObjectURL(files[0]);
+
+      toDataURL(
+        node.image,
+        function(dataUrl) {
+          node.image = dataUrl;
+        }
+      )
+
            
             /*
             let newNode = addNode({label:'Validate', nodeClass:'Validator', subject:node});
@@ -26,13 +55,12 @@
 {#if node}
   
     <input
-		type="file"
-		accept="image/*"
-		class="w-full h-full cursor-pointer"
-		bind:files
-		on:change={changed}
-    />
-    <input type="text" bind:value={node.image}>
+      type="file"
+      accept="image/*"
+      class="w-full h-full cursor-pointer"
+      bind:files
+      on:change={changed}
+      />
     <input type=range bind:value={node.size} min=45 max=200 step=5><span>&nbsp;{node.size}</span>
 
 {/if}
